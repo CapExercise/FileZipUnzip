90ec3bd87c2f7847c23e8fa4997b72a6859a76f8
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-19 12:06:14 +0530

    packages and implementations for encoding using top words within file created

---
+++ src/main/java/com/capexercise/huffman/topword/TopWordZipperUnZipper.java
@@ -0,0 +1,116 @@
+package com.capexercise.huffman.topword;
+
+import com.capexercise.filezipunzip.FileZipper;
+import com.capexercise.general.FileContents;
+import com.capexercise.general.FrequencyComparator;
+import com.capexercise.general.IFileContents;
+import com.capexercise.general.Path;
+import com.capexercise.general.helpers.input.FileHandler;
+import com.capexercise.general.helpers.input.IDataHandle;
+import com.capexercise.general.helpers.maps.IMap;
+import com.capexercise.general.helpers.maps.WordMaps;
+import com.capexercise.general.helpers.nodes.StringTreeNode;
+import com.capexercise.general.helpers.nodes.TreeNode;
+import com.capexercise.huffman.compression.ICompress;
+import com.capexercise.huffman.decompression.IDecompress;
+import com.capexercise.huffman.general.GeneralMethods;
+import com.capexercise.huffman.general.IGeneral;
+import com.capexercise.huffman.word.compressor.WordCompress;
+import com.capexercise.huffman.word.decompressor.WordDecompress;
+
+import java.io.File;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopWordZipperUnZipper implements FileZipper {
+
+    IGeneral method;
+
+    public TopWordZipperUnZipper(){
+        method = new GeneralMethods();
+    }
+
+
+    @Override
+    public void compress() {
+        ICompress compressor = new WordCompress();
+
+        IDataHandle dataObj = new FileHandler(Path.inputFilePath);
+
+        IMap compressionMaps = compressor.calculateFreq(dataObj);
+
+        TreeNode root = this.constructTree(compressionMaps);
+
+        compressor.iterateTreeAndCalculateHuffManCode(root, "", compressionMaps);
+
+        StringBuilder coded = compressor.getCodes(compressionMaps, dataObj);
+
+        int noOfZerosAppended = compressor.noofZerosToBeAppended(coded);
+
+        if (noOfZerosAppended != 0)
+            coded = compressor.appendRemainingZeros(coded);
+
+        byte[] byteArray = compressor.compress(coded);
+
+        compressionMaps.clearHuffMap();
+
+        IFileContents fileContents = new FileContents((Map<Object, Integer>) compressionMaps.returnMap(), noOfZerosAppended, byteArray);
+        method.addCompressedContents(fileContents);
+
+
+        System.out.println("Compression done Successfully");
+    }
+
+    @Override
+    public void decompress() {
+        IDecompress decompressor = new WordDecompress();
+        IFileContents fileContents = method.extractContents(new File(Path.compressedFilePath));
+        Map<Object, Integer> freq = fileContents.getFrequencyMap();
+        int noOfZeros = fileContents.getExtraBits();
+        byte[] byteArray = fileContents.getByteArray();
+
+        IMap decompressionMap = new WordMaps(freq, null);
+        TreeNode root = this.constructTree(decompressionMap);
+        StringBuilder decoded = decompressor.getDecodedString(byteArray);
+        decompressor.writeIntoDecompressedFile(root, decoded, noOfZeros);
+
+
+        System.out.println("De-Compression done Successfully");
+
+
+        method.displayStats(Path.inputFilePath, Path.compressedFilePath, Path.decompressedFilePath);
+    }
+
+    @Override
+    public TreeNode constructTree(IMap imap) {
+        PriorityQueue<TreeNode> pq = new PriorityQueue<>(imap.freqSize(), new FrequencyComparator());
+        Map<Object, Integer> freq = (Map<Object, Integer>) imap.returnMap();
+
+        for (Map.Entry<Object, Integer> entry : freq.entrySet()) {
+            TreeNode node = new StringTreeNode((String) entry.getKey(), entry.getValue());
+            pq.add(node);
+        }
+        TreeNode root = null;
+        if (pq.size() == 1) {
+            TreeNode leftSideNode = pq.peek();
+            pq.poll();
+            TreeNode newNode = new StringTreeNode("-", leftSideNode.getFrequency());
+            newNode.setLeft(leftSideNode);
+            newNode.setRight(null);
+            root = newNode;
+            return root;
+        }
+        while (pq.size() > 1) {
+            TreeNode leftSideNode = pq.poll();
+
+            TreeNode rightSideNode = pq.poll();
+            TreeNode newNode = new StringTreeNode("-", leftSideNode.getFrequency() + rightSideNode.getFrequency());
+            newNode.setLeft(leftSideNode);
+            newNode.setRight(rightSideNode);
+            root = newNode;
+            pq.add(newNode);
+        }
+        return root;
+    }
+
+}
---
+++ src/main/java/com/capexercise/huffman/topword/compressor/TopWordCompress.java
@@ -0,0 +1,96 @@
+package com.capexercise.huffman.topword.compressor;
+
+import com.capexercise.general.helpers.input.IDataHandle;
+import com.capexercise.general.helpers.maps.IMap;
+import com.capexercise.general.helpers.maps.WordMaps;
+import com.capexercise.general.helpers.nodes.TreeNode;
+import com.capexercise.huffman.compression.ICompress;
+
+public class TopWordCompress implements ICompress {
+    @Override
+    public IMap calculateFreq(IDataHandle dataObj) {
+        IMap imap = new WordMaps();
+
+        String[] strData = dataObj.readContentAsArray();
+
+        for (String str : strData) {
+            imap.putFrequency(str, imap.getFrequency(str));
+        }
+
+        return imap;
+    }
+
+    @Override
+    public StringBuilder appendRemainingZeros(StringBuilder coded) {
+        int rem = coded.length() % 8;
+        if (rem != 0) {
+            rem = 8 - rem;
+            while (rem != 0) {
+                coded = coded.append("0");
+                rem--;
+            }
+        }
+        return coded;
+    }
+
+    @Override
+    public int noofZerosToBeAppended(StringBuilder coded) {
+        if (coded.length() == 0 | coded.length() % 8 == 0) {
+            return 0;
+        }
+        return 8 - (coded.length() % 8);
+    }
+
+    @Override
+    public byte[] compress(StringBuilder coded) {
+        byte[] bytearray = new byte[coded.length() / 8];
+        StringBuilder sub = new StringBuilder();
+        int bytearrayIndex = 0;
+        for (int i = 0; i < coded.length(); i = i + 8) {
+            int j = 0;
+            while (j < 8) {
+                sub = sub.append(coded.charAt(i + j));
+                j++;
+            }
+            bytearray[bytearrayIndex] = (byte) Integer.parseInt(sub.toString(), 2);
+            bytearrayIndex++;
+            sub.setLength(0);
+        }
+        return bytearray;
+    }
+
+
+    @Override
+    public void iterateTreeAndCalculateHuffManCode(TreeNode newNode, String s, IMap huffmanMap) {
+        if (newNode == null) {
+            return;
+        }
+        if (newNode.getLeft() == null && newNode.getRight() == null) {
+            huffmanMap.putHuffManCode(newNode.getVar(), s);
+        }
+        iterateTreeAndCalculateHuffManCode(newNode.getLeft(), s + "0", huffmanMap);
+        iterateTreeAndCalculateHuffManCode(newNode.getRight(), s + "1", huffmanMap);
+    }
+
+    @Override
+    public StringBuilder getCodes(IMap huffmanMap, IDataHandle fobj) {
+        StringBuilder finalAns = new StringBuilder();
+        String ans = fobj.readContent();
+        String sub = "";
+        for (int i = 0; i < ans.length(); i++) {
+            while (i < ans.length() && (Character.isAlphabetic(ans.charAt(i)) || Character.isDigit(ans.charAt(i))))
+                sub += ans.charAt(i++);
+
+
+            if (huffmanMap.containsHuffKey(sub))
+                finalAns.append(huffmanMap.getHuffmanCode(sub));
+            if (huffmanMap.containsHuffKey(String.valueOf(ans.charAt(i))))
+                finalAns.append(huffmanMap.getHuffmanCode(String.valueOf(ans.charAt(i))));
+
+            sub = "";
+
+        }
+        return finalAns;
+    }
+
+}
