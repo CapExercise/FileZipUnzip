2821133532bbecedbf548f84b52946873827b482
Dinesh Kudva <dineshkudva12@gmail.com>
2023-02-20 12:39:38 +0530

    test cases for FileHandler class added

---
+++ src/test/java/com/capexercise/general/helpers/input/FileHandlerTest.java
@@ -0,0 +1,45 @@
+package com.capexercise.general.helpers.input;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileHandlerTest {
+    IDataHandle testRef;
+
+    @Before
+    public void setup(){
+        testRef = new FileHandler("src/main/java/com/capexercise/Files/empty.txt");
+    }
+
+    @Test
+    public void testReadContentForEmpty(){
+        String actual = testRef.readContent().toString();
+        String expected = "";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testReadContent() {
+        testRef = new FileHandler("src/main/java/com/capexercise/Files/testFile.txt");
+        String actual = testRef.readContent().toString();
+        String expected = "Hello world.This is it.";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testReadContentAsArrayForEmpty(){
+        testRef = new FileHandler("src/main/java/com/capexercise/Files/empty.txt");
+        String[] actual = testRef.readContentAsArray();
+        String[] expected = new String[0];
+        assertArrayEquals(expected,actual);
+    }
+    @Test
+    public void testReadContentAsArray(){
+        testRef = new FileHandler("src/main/java/com/capexercise/Files/testFile.txt");
+        String[] actual = testRef.readContentAsArray();
+        String[] expected = new String[]{"Hello"," ","world",".","This"," ","is"," ","it","."};
+        assertArrayEquals(expected,actual);
+    }
+}
