bf247aa45cedcdaef1b0792306ac812f3e9221c0
Dinesh Kudva <dineshkudva12@gmail.com>
2023-03-03 14:40:19 +0530

    minor change

--- src/main/java/com/capexercise/general/Path.java
+++ src/main/java/com/capexercise/general/Path.java
@@ -3,6 +3,7 @@ package com.capexercise.general;
 public interface Path
 {
     String compressedFilePath = "src/main/java/com/capexercise/Files/compressed/compressed.txt";
+
     String decompressedFilePath = "src/main/java/com/capexercise/Files/decompressed/decompressed.txt";
     String inputFilePath = "src/main/java/com/capexercise/Files/input.txt";
 }
