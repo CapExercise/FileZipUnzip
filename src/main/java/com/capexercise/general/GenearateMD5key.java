eb1cdf91e1d7924e2d7abc7fe1bc4d1ac8bd66c1
rohanhegde <rohanhegde999@gmail.com>
2023-03-03 16:26:23 +0530

    implementation for generating MD5 key for a file

--- src/main/java/com/capexercise/general/GenearateMD5key.java
+++ src/main/java/com/capexercise/general/GenearateMD5key.java
@@ -12,9 +12,43 @@ public class GenearateMD5key
     {
         File file = new File(path);
         MessageDigest mdigest = MessageDigest.getInstance("MD5");
-       return checksum(mdigest, file);
-    }
 
+
+
+        FileInputStream fis = new FileInputStream(file);
+
+
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+
+        while ((bytesCount = fis.read(byteArray)) != -1)
+        {
+            mdigest.update(byteArray, 0, bytesCount);
+        };
+
+
+        fis.close();
+
+
+        byte[] bytes = mdigest.digest();
+
+
+        StringBuilder sb = new StringBuilder();
+
+        // loop through the bytes array
+        for (int i = 0; i < bytes.length; i++) {
+
+
+            sb.append(Integer
+                    .toString((bytes[i] & 0xff) + 0x100, 16)
+                    .substring(1));
+        }
+
+
+        return sb.toString();
+    }
+/*
     private static String checksum(MessageDigest digest,
                                    File file)
             throws IOException
@@ -53,4 +87,6 @@ public class GenearateMD5key
       
         return sb.toString();
     }
+
+ */
 }
