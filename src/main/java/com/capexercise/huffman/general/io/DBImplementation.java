d50760229903687ff3f61a38127b83ced5a5547b
Dinesh Kudva <dineshkudva12@gmail.com>
2023-03-04 02:35:33 +0530

    DBImplementation updated

--- src/main/java/com/capexercise/huffman/general/io/DBImplementation.java
+++ src/main/java/com/capexercise/huffman/general/io/DBImplementation.java
@@ -52,8 +52,6 @@ public class DBImplementation implements InputOutput {
             obj.close();
             fout.close();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
