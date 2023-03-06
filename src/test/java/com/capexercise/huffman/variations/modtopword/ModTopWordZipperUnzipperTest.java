4309bb5641951c438639bc94667f4f79e8231ddc
rohanhegde <rohanhegde999@gmail.com>
2023-03-07 00:43:14 +0530

    new test cases added

--- src/test/java/com/capexercise/huffman/variations/modtopword/ModTopWordZipperUnzipperTest.java
+++ src/test/java/com/capexercise/huffman/variations/modtopword/ModTopWordZipperUnzipperTest.java
@@ -4,8 +4,10 @@ import com.capexercise.filezipunzip.FileZipper;
 import com.capexercise.general.helpers.maps.IMap;
 import com.capexercise.general.helpers.maps.WordMaps;
 import com.capexercise.general.helpers.nodes.TreeNode;
+import com.capexercise.huffman.general.database.IDataBase;
 import com.capexercise.huffman.variations.topword.TopWordZipperUnZipper;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -16,27 +18,37 @@ import static org.junit.Assert.*;
 
 public class ModTopWordZipperUnzipperTest
 {
-/*
+
 
     ModTopWordZipperUnzipper testRef=new ModTopWordZipperUnzipper();
-    @Test
+
+    IDataBase db;
+
+    @Before
     public void test() throws SQLException, IOException, ClassNotFoundException {
 
         FileZipper mySpy= Mockito.spy(ModTopWordZipperUnzipper.class);
         mySpy.compress();
         Mockito.verify(mySpy, Mockito.times(1)).compress();
     }
-
+/*
     @After
     public void testDecompress() throws SQLException, IOException, ClassNotFoundException {
+    db=new SQLImplemenation();
         FileZipper mySpy= Mockito.spy(ModTopWordZipperUnzipper.class);
         mySpy.decompress();
         Mockito.verify(mySpy, Mockito.times(1)).decompress();
     }
 
+
+
+ */
+
     @Test
     public void testConstructTree()
+
     {
+
         IMap map=new WordMaps();
         map.putFrequency("a",1);
         TreeNode root= testRef.constructTree(map);
@@ -46,5 +58,5 @@ public class ModTopWordZipperUnzipperTest
     }
 
 
- */
+
 }
